ee3fed4fdaa75456d9471cda0f4e06ab8382ef95
Sulu Edward Julianto <93524287+sulujulianto@users.noreply.github.com>
2025-10-20 15:44:09 +0700

    Update Commit

---
+++ Pengembangan-Web/Javascript/Bu Jeanny/Jobsheet/latihan-js-dasar/komentar.js
@@ -0,0 +1,32 @@
+// ini adalah komentar satu baris
+// komentar tidak akan dijalankan oleh program
+
+console.log('Belajar JavaScript');
+
+// variabel untuk menyimpan nama
+let nama = 'Ahmad';
+
+/*
+ini adalah komentar
+dengan banyak baris
+bisa digunakan untuk
+dokumentasi panjang
+*/
+
+console.log('Nama:', nama);
+
+// menghitung luas persegi
+let sisi = 5;
+let luas = sisi * sisi; // rumus luas = sisi x sisi
+console.log('Luas persegi:', luas);
+
+/*
+Komentar berguna untuk:
+1. Menjelaskan kode
+2. Menonaktifkan kode sementara
+3. Dokumentasi program
+*/
+
+// console.log('Ini tidak akan muncul');
+
+console.log('Program selesai');
