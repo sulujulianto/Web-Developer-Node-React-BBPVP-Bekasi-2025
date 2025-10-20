f388565baa1b49ddd61a728f75864eb05b67bdea
Sulu Edward Julianto <93524287+sulujulianto@users.noreply.github.com>
2025-10-20 15:43:11 +0700

    Update Commit

---
+++ Pengembangan-Web/Javascript/Bu Jeanny/Jobsheet/latihan-js-dasar/tipe-data.js
@@ -0,0 +1,49 @@
+// tipe data string
+let nama = 'Budi Santoso';
+let alamat = "Jakarta";
+console.log('Nama:', nama);
+console.log('Alamat:', alamat);
+console.log('');
+
+// tipe data number
+let umur = 25;
+let tinggi = 175.5;
+let berat = 68;
+console.log('Umur:', umur, 'tahun');
+console.log('Tinggi:', tinggi, 'cm');
+console.log('Berat:', berat, 'kg');
+console.log('');
+
+// tipe data boolean
+let sudahMenikah = false;
+let punyaSIM = true;
+console.log('Sudah menikah:', sudahMenikah);
+console.log('Punya SIM:', punyaSIM);
+console.log('');
+
+// tipe data null
+let mobil = null;
+console.log('Mobil:', mobil);
+console.log('');
+
+// tipe data undefined
+let motor;
+console.log('Motor:', motor);
+console.log('');
+
+// tipe data array
+let hobi = ['membaca', 'olahraga', 'coding'];
+let angka = [1, 2, 3, 4, 5];
+console.log('Hobi:', hobi);
+console.log('Angka:', angka);
+console.log('');
+
+// tipe data object
+let mahasiswa = {
+    nama: 'Andi',
+    nim: '12345',
+    jurusan: 'Informatika'
+};
+console.log('Data Mahasiswa:', mahasiswa);
+console.log('Nama:', mahasiswa.nama);
+console.log('NIM:', mahasiswa.nim);
