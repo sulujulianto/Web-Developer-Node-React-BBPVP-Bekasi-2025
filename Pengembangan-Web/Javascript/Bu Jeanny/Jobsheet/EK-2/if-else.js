e2b6d1dc2ce625f9fbbe71302e179f14ad53f244
Sulu Edward Julianto <93524287+sulujulianto@users.noreply.github.com>
2025-10-22 12:42:59 +0700

    Update Commit

--- Pengembangan-Web/Javascript/Bu Jeanny/Jobsheet/EK-2/if-else.js
+++ Pengembangan-Web/Javascript/Bu Jeanny/Jobsheet/EK-2/if-else.js
@@ -1,51 +1,11 @@
-// contoh percabangan if
-console.log('=== PERCABANGAN IF ===');
-let nilai = 80;
+let nilai = 83;
 
-if (nilai >= 75) {
-    console.log('Selamat, kamu lulus!');
-}
-console.log('');
-
-// contoh if else
-console.log('=== PERCABANGAN IF ELSE ===');
-let umur = 17;
-
-if (umur >= 18) {
-    console.log('Anda sudah dewasa');
+if (nilai >= 90) {
+    console.log("Nilai kamu A");
+} else if (nilai >= 80) {
+    console.log("Nilai kamu B");
+} else if (nilai >= 70) {
+    console.log("Nilai kamu C");
 } else {
-    console.log('Anda masih anak-anak');
-}
-console.log('');
-
-// contoh if else if
-console.log('=== PERCABANGAN IF ELSE IF ===');
-let score = 85;
-
-if (score >= 90) {
-    console.log('Nilai: A');
-} else if (score >= 80) {
-    console.log('Nilai: B');
-} else if (score >= 70) {
-    console.log('Nilai: C');
-} else if (score >= 60) {
-    console.log('Nilai: D');
-} else {
-    console.log('Nilai: E');
-}
-console.log('');
-
-// contoh nested if
-console.log('=== NESTED IF ===');
-let username = 'admin';
-let password = '12345';
-
-if (username == 'admin') {
-    if (password == '12345') {
-        console.log('Login berhasil');
-    } else {
-        console.log('Password salah');
-    }
-} else {
-    console.log('Username tidak ditemukan');
+    console.log("Nilai kamu D");
 }
