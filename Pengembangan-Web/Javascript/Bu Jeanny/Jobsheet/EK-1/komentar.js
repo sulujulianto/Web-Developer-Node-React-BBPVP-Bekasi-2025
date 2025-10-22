b618d8ac479db44480ff35dc51bf7d09b6223b08
Sulu Edward Julianto <93524287+sulujulianto@users.noreply.github.com>
2025-10-22 11:58:04 +0700

    Update Commit

--- Pengembangan-Web/Javascript/Bu Jeanny/Jobsheet/EK-1/komentar.js
+++ Pengembangan-Web/Javascript/Bu Jeanny/Jobsheet/EK-1/komentar.js
@@ -1,32 +1,9 @@
-// ini adalah komentar satu baris
-// komentar tidak akan dijalankan oleh program
-
-console.log('Belajar JavaScript');
-
-// variabel untuk menyimpan nama
-let nama = 'Ahmad';
+// Komentar satu baris
 
 /*
-ini adalah komentar
-dengan banyak baris
-bisa digunakan untuk
-dokumentasi panjang
+Komentar beberapa baris
+Menjelaskan bagian program
 */
 
-console.log('Nama:', nama);
-
-// menghitung luas persegi
-let sisi = 5;
-let luas = sisi * sisi; // rumus luas = sisi x sisi
-console.log('Luas persegi:', luas);
-
-/*
-Komentar berguna untuk:
-1. Menjelaskan kode
-2. Menonaktifkan kode sementara
-3. Dokumentasi program
-*/
-
-// console.log('Ini tidak akan muncul');
-
-console.log('Program selesai');
+let nama = "Lavender"; // Menyimpan nama siswa
+console.log("Nama saya", nama); // Tampilkan ke layar
