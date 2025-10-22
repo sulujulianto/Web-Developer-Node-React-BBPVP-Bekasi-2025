7d355ffabfa6a4533fb2d3593026952f16929fb6
Sulu Edward Julianto <93524287+sulujulianto@users.noreply.github.com>
2025-10-22 11:57:01 +0700

    Update Commit

--- Pengembangan-Web/Javascript/Bu Jeanny/Jobsheet/EK-1/tipe-data.js
+++ Pengembangan-Web/Javascript/Bu Jeanny/Jobsheet/EK-1/tipe-data.js
@@ -1,49 +1,15 @@
-// tipe data string
-let nama = 'Budi Santoso';
-let alamat = "Jakarta";
-console.log('Nama:', nama);
-console.log('Alamat:', alamat);
-console.log('');
-
-// tipe data number
+let nama = "Daisy";
 let umur = 25;
-let tinggi = 175.5;
-let berat = 68;
-console.log('Umur:', umur, 'tahun');
-console.log('Tinggi:', tinggi, 'cm');
-console.log('Berat:', berat, 'kg');
-console.log('');
+let lulus = true;
+let nilai = null;
+let alamat;
+let hobi = ["Baca", "Nyanyi"];
+let siswa = { nama: "Rose", kelas: "Web" };
 
-// tipe data boolean
-let sudahMenikah = false;
-let punyaSIM = true;
-console.log('Sudah menikah:', sudahMenikah);
-console.log('Punya SIM:', punyaSIM);
-console.log('');
-
-// tipe data null
-let mobil = null;
-console.log('Mobil:', mobil);
-console.log('');
-
-// tipe data undefined
-let motor;
-console.log('Motor:', motor);
-console.log('');
-
-// tipe data array
-let hobi = ['membaca', 'olahraga', 'coding'];
-let angka = [1, 2, 3, 4, 5];
-console.log('Hobi:', hobi);
-console.log('Angka:', angka);
-console.log('');
-
-// tipe data object
-let mahasiswa = {
-    nama: 'Andi',
-    nim: '12345',
-    jurusan: 'Informatika'
-};
-console.log('Data Mahasiswa:', mahasiswa);
-console.log('Nama:', mahasiswa.nama);
-console.log('NIM:', mahasiswa.nim);
+console.log("Nama:", nama);
+console.log("Umur:", umur);
+console.log("Lulus:", lulus);
+console.log("Nilai:", nilai);
+console.log("Alamat:", alamat);
+console.log("Hobi:", hobi);
+console.log("Siswa:", siswa);
